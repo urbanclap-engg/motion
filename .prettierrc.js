73403b58ba6298819de42e36b2e9225c76eeabcb
Gaurav verma <gauravverma@urbancompany.com>
2022-07-12 11:08:36 +0530

    refactor(motion): gitigone added and files updated

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 80,
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: true,
+    useTabs: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    parser: 'typescript',
+    jsxBracketSameLine: false
+  };
+  
